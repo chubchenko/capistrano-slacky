4395e623e0c4787829b7b5a1a4b702666dac8cd6
Artem Chubchenko <artem.chubchenko@gmail.com>
2021-05-27 14:02:02 +0300

    Skip sending a message to a Slack if the payload is empty (#2)

--- lib/capistrano/slacky/payload.rb
+++ lib/capistrano/slacky/payload.rb
@@ -11,14 +11,24 @@ module Capistrano
         @messaging = ::Capistrano::Slacky::Messaging.for(env: env)
       end
 
+      def empty?
+        payload_for_action.nil?
+      end
+
       def to_json
         {
           username: ::Capistrano::Slacky.username,
           icon_emoji: ::Capistrano::Slacky.icon_emoji,
           channel: ::Capistrano::Slacky.channel
         }.merge(
-          @messaging.payload_for(action: @action)
+          payload_for_action
         ).to_json
+      end
+
+      private
+
+      def payload_for_action
+        @payload_for_action ||= @messaging.payload_for(action: @action)
       end
     end
   end
