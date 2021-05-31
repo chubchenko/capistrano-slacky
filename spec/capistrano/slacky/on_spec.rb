a0ed78c0b9fbfc681a1762f9e392aca5ef7ecb59
chubchenko <artem.chubchenko@gmail.com>
2021-05-31 23:09:22 +0300

    Add tests for the Capistrano::Slacky::On

---
+++ spec/capistrano/slacky/on_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe Capistrano::Slacky::On do
+  describe ".on" do
+    let(:backend) { instance_double(SSHKit::Backend::Netssh) }
+
+    before do
+      Capistrano::Configuration.env.server("example.com", roles: %w[app], primary: true)
+
+      allow(Capistrano::DSL).to receive(:on)
+        .with(Capistrano::Configuration.env.primary(:app))
+        .and_yield
+    end
+
+    context "when the within the repository" do
+      before do
+        allow(SSHKit::Backend).to receive(:current).and_return(backend)
+
+        allow(backend).to receive(:within).with(Pathname.new("var/www/slacky/repo"))
+          .and_yield
+      end
+
+      it "yields on the remote server in the `var/www/slacky/repo` folder" do
+        expect do |block|
+          described_class.on(within: :repository, &block)
+        end.to yield_control
+      end
+    end
+
+    context "when the within the shared" do
+      before do
+        allow(SSHKit::Backend).to receive(:current).and_return(backend)
+
+        allow(backend).to receive(:within).with(Pathname.new("var/www/slacky/shared"))
+          .and_yield
+      end
+
+      it "yields on the remote server in the `var/www/slacky/shared` folder" do
+        expect do |block|
+          described_class.on(within: :shared, &block)
+        end.to yield_control
+      end
+    end
+  end
+end
