a541e7978822580980f4f334a577e75177dc394a
chubchenko <artem.chubchenko@gmail.com>
2021-05-31 20:30:46 +0300

    Add tests for the Capistrano::Slacky::Fanout

---
+++ spec/capistrano/slacky/fanout_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe Capistrano::Slacky::Fanout do
+  describe ".call" do
+    let(:backend) { instance_double(SSHKit::Backend::Netssh) }
+
+    before do
+      allow(Capistrano::Slacky::On).to receive(:on)
+        .with(within: :shared)
+        .and_yield
+    end
+
+    before do
+      allow(SSHKit::Backend).to receive(:current).and_return(backend)
+      allow(backend).to receive(:capture).with(:cat, "config/slacky.yml").and_return(
+        "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+      )
+
+      allow(Net::HTTP).to receive(:post_form)
+    end
+
+    let(:payload) do
+      {username: "ChatOps", text: "Hello, Slacky!"}
+    end
+
+    it "makes an HTTP POST request" do
+      described_class.call(payload: payload)
+
+      expect(Net::HTTP).to have_received(:post_form).with(
+        URI("https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"),
+        payload: "{\"username\":\"ChatOps\",\"text\":\"Hello, Slacky!\"}"
+      )
+    end
+  end
+end
