3e70de1fea13c6c028d026ad76e136da8e2e3850
chubchenko <artem.chubchenko@gmail.com>
2021-05-31 20:05:20 +0300

    Add tests for the Capistrano::Slacky::Messaging module

---
+++ spec/capistrano/slacky/messaging/default_spec.rb
@@ -0,0 +1,215 @@
+# frozen_string_literal: true
+
+RSpec.describe Capistrano::Slacky::Messaging::Default do
+  describe "#payload_for_updated" do
+    before { allow(Capistrano::Slacky::On).to receive(:on).with(within: :repository).and_yield }
+
+    it "returns the Hash representation for a successful deployment" do
+      expect(described_class.new(env: Capistrano::Configuration.env).payload_for_updated).to match(
+        {
+          blocks: [
+            {
+              type: :header,
+              text: {
+                type: :plain_text,
+                text: a_string_matching(/^Deployed successfully :.+:$/),
+                emoji: true
+              }
+            },
+            {
+              type: :section,
+              fields: [
+                {type: :mrkdwn, text: ">*Stage:*"},
+                {type: :mrkdwn, text: "`production`"},
+                {type: :mrkdwn, text: ">*Branch:*"},
+                {type: :mrkdwn, text: "`master`"},
+                {type: :mrkdwn, text: ">*Duration:*"},
+                {type: :mrkdwn, text: a_string_matching(/`\d{2}:\d{2}`/)}
+              ]
+            },
+            {
+              type: :context,
+              elements: [
+                {
+                  type: :mrkdwn,
+                  text: "*Current* revision <https://github.com/chubchenko/capistrano-slacky/commit/02c4c96|02c4c96>" \
+                        " - " \
+                        "<https://github.com/chubchenko/capistrano-slacky/compare/2eab27b...02c4c96|compare>" \
+                        " (_previous revision was " \
+                        "<https://github.com/chubchenko/capistrano-slacky/commit/2eab27b|2eab27b>_)."
+                }
+              ]
+            },
+            {
+              type: :context,
+              elements: [
+                {type: :mrkdwn, text: ":one:"},
+                {type: :mrkdwn, text: "<https://github.com/chubchenko/capistrano-slacky/commit/02c4c96|02c4c96>"},
+                {type: :mrkdwn, text: "Update changelog & dependency version"}
+              ]
+            },
+            {
+              type: :context,
+              elements: [
+                {type: :mrkdwn, text: ":two:"},
+                {type: :mrkdwn, text: "<https://github.com/chubchenko/capistrano-slacky/commit/705253c|705253c>"},
+                {type: :mrkdwn, text: "Build initial version of the gem (#1)"}
+              ]
+            },
+            {
+              type: :context,
+              elements: [
+                {type: :mrkdwn, text: ":truck: Deployed By: @chubchenko"}
+              ]
+            }
+          ]
+        }
+      )
+    end
+  end
+
+  describe "#payload_for_reverted" do
+    it "returns the Hash representation for a successful rollback" do
+      expect(described_class.new(env: Capistrano::Configuration.env).payload_for_reverted).to match(
+        {
+          blocks: [
+            {
+              type: :header,
+              text: {
+                type: :plain_text,
+                text: a_string_matching(/^Reverted successfully :.+:$/),
+                emoji: true
+              }
+            },
+            {
+              type: :section,
+              fields: [
+                {type: :mrkdwn, text: ">*Stage:*"},
+                {type: :mrkdwn, text: "`production`"},
+                {type: :mrkdwn, text: ">*Branch:*"},
+                {type: :mrkdwn, text: "`master`"},
+                {type: :mrkdwn, text: ">*Duration:*"},
+                {type: :mrkdwn, text: a_string_matching(/`\d{2}:\d{2}`/)}
+              ]
+            },
+            {
+              type: :context,
+              elements: [
+                {
+                  type: :mrkdwn,
+                  text: "*Current* revision <https://github.com/chubchenko/capistrano-slacky/commit/02c4c96|02c4c96>" \
+                        " - " \
+                        "<https://github.com/chubchenko/capistrano-slacky/compare/2eab27b...02c4c96|compare>" \
+                        " (_previous revision was " \
+                        "<https://github.com/chubchenko/capistrano-slacky/commit/2eab27b|2eab27b>_)."
+                }
+              ]
+            },
+            {
+              type: :context,
+              elements: [
+                {type: :mrkdwn, text: ":truck: Deployed By: @chubchenko"}
+              ]
+            }
+          ]
+        }
+      )
+    end
+  end
+
+  describe "#payload_for_failed" do
+    let(:env) { Capistrano::Configuration.env }
+
+    context "when deployment failed" do
+      around do |example|
+        env.set(:deploying, true)
+
+        example.run
+
+        env.set(:deploying, false)
+      end
+
+      it "returns the Hash representation for a failed deployment" do
+        expect(described_class.new(env: env).payload_for_failed).to match(
+          {
+            blocks: [
+              {
+                type: :header,
+                text: {
+                  type: :plain_text,
+                  text: a_string_matching(/Deployment failed :.+:$/),
+                  emoji: true
+                }
+              },
+              {
+                type: :section,
+                fields: [
+                  {type: :mrkdwn, text: ">*Stage:*"},
+                  {type: :mrkdwn, text: "`production`"},
+                  {type: :mrkdwn, text: ">*Branch:*"},
+                  {type: :mrkdwn, text: "`master`"},
+                  {type: :mrkdwn, text: ">*Duration:*"},
+                  {type: :mrkdwn, text: a_string_matching(/`\d{2}:\d{2}`/)}
+                ]
+              },
+              {
+                type: :context,
+                elements: [
+                  {type: :mrkdwn, text: "Something went wrong and we couldn't get the exception to display :sob:."}
+                ]
+              },
+              {
+                type: :context,
+                elements: [
+                  {type: :mrkdwn, text: ":truck: Deployed By: @chubchenko"}
+                ]
+              }
+            ]
+          }
+        )
+      end
+    end
+
+    context "when rollback failed" do
+      it "returns the Hash representation for a failed rollback" do
+        expect(described_class.new(env: env).payload_for_failed).to match(
+          {
+            blocks: [
+              {
+                type: :header,
+                text: {
+                  type: :plain_text,
+                  text: a_string_matching(/Rollback failed :.+:$/),
+                  emoji: true
+                }
+              },
+              {
+                type: :section,
+                fields: [
+                  {type: :mrkdwn, text: ">*Stage:*"},
+                  {type: :mrkdwn, text: "`production`"},
+                  {type: :mrkdwn, text: ">*Branch:*"},
+                  {type: :mrkdwn, text: "`master`"},
+                  {type: :mrkdwn, text: ">*Duration:*"},
+                  {type: :mrkdwn, text: a_string_matching(/`\d{2}:\d{2}`/)}
+                ]
+              },
+              {
+                type: :context,
+                elements: [
+                  {type: :mrkdwn, text: "Something went wrong and we couldn't get the exception to display :sob:."}
+                ]
+              },
+              {
+                type: :context,
+                elements: [
+                  {type: :mrkdwn, text: ":truck: Deployed By: @chubchenko"}
+                ]
+              }
+            ]
+          }
+        )
+      end
+    end
+  end
+end
---
+++ spec/capistrano/slacky/messaging/null_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+RSpec.describe Capistrano::Slacky::Messaging::Null do
+  describe "#payload_for" do
+    subject(:null) { described_class.new(env: Capistrano::Configuration.env) }
+
+    context "when action is `updated`" do
+      before do
+        allow(null).to receive(:public_send).with("payload_for_updated")
+
+        null.payload_for(action: :updated)
+      end
+
+      it { expect(null).to have_received(:public_send).with("payload_for_updated") }
+    end
+
+    context "when action is `reverted`" do
+      before do
+        allow(null).to receive(:public_send).with("payload_for_reverted")
+
+        null.payload_for(action: :reverted)
+      end
+
+      it { expect(null).to have_received(:public_send).with("payload_for_reverted") }
+    end
+
+    context "when action is `failed`" do
+      before do
+        allow(null).to receive(:public_send).with("payload_for_failed")
+
+        null.payload_for(action: :failed)
+      end
+
+      it { expect(null).to have_received(:public_send).with("payload_for_failed") }
+    end
+
+    context "when the action is unpermitted" do
+      it "raises an exception that action is unpermitted" do
+        expect do
+          null.payload_for(action: :unpermitted)
+        end.to raise_error(
+          Capistrano::Slacky::Messaging::UnpermittedAction,
+          "Unpermitted action: unpermitted."
+        )
+      end
+    end
+  end
+
+  describe "#payload_for_updated" do
+    subject(:null) { described_class.new(env: Capistrano::Configuration.env) }
+
+    it { expect(null.payload_for_updated).to be_nil }
+  end
+
+  describe "#payload_for_reverted" do
+    subject(:null) { described_class.new(env: Capistrano::Configuration.env) }
+
+    it { expect(null.payload_for_reverted).to be_nil }
+  end
+
+  describe "#payload_for_failed" do
+    subject(:null) { described_class.new(env: Capistrano::Configuration.env) }
+
+    it { expect(null.payload_for_failed).to be_nil }
+  end
+end
